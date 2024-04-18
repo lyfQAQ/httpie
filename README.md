df8562d31136ad7d1a1ff5b71b9a55dd97d264ac
lyfQAQ <0219lyf@gmail.com>
2024-04-18 11:22:02 +0800

    final

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 # httpie
-- 命令行解析: [clap](https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_2/index.html#subcommands)
+- 命令行解析
 - 发送 http 请求
 - 用户友好的方式输出
+
+```
+~/Desktop/work/github/httpie master !1 ❯ ./target/release/httpie post https://httpbin.org/post greeting=hola name=liuyifan
+HTTP/1.1 200 OK
+
+date: "Thu, 18 Apr 2024 03:20:16 GMT"
+content-type: "application/json"
+content-length: "512"
+connection: "keep-alive"
+server: "gunicorn/19.9.0"
+access-control-allow-origin: "*"
+access-control-allow-credentials: "true"
+
+
+{
+  "args": {},
+  "data": "{\"name\":\"liuyifan\",\"greeting\":\"hola\"}",
+  "files": {},
+  "form": {},
+  "headers": {
+    "Accept": "*/*",
+    "Content-Length": "37",
+    "Content-Type": "application/json",
+    "Host": "httpbin.org",
+    "User-Agent": "Rust Httpie",
+    "X-Amzn-Trace-Id": "Root=1-66209170-640b5558752fcb2376261a47",
+    "X-Powerd-By": "Rust"
+  },
+  "json": {
+    "greeting": "hola",
+    "name": "liuyifan"
+  },
+  "origin": "103.182.96.100",
+  "url": "https://httpbin.org/post"
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, Result};
 use clap::{Args, Parser, Subcommand};
-use reqwest::{Client, Url};
+use colored::*;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
 use std::{collections::HashMap, str::FromStr};
-
 /// A naive httpie implementation with Rust
 #[derive(Parser, Debug)]
 #[command(version = "1.0", about, long_about = None)]
@@ -43,7 +44,7 @@ struct Post {
 }
 
 /// 通过 parse_kv_pair 将key=value 解析成 KvPair 结构
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
@@ -77,8 +78,11 @@ fn parse_kv_pair(s: &str) -> Result<KvPair> {
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-POWERD-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
     // 生成 http 客户端
-    let client = Client::new();
+    let client = Client::builder().default_headers(headers).build()?;
     let result = match opts.subcmd {
         SubCommand::Get(ref args) => get(client, args).await?,
         SubCommand::Post(ref args) => post(client, args).await?,
@@ -86,18 +90,90 @@ async fn main() -> Result<()> {
     Ok(result)
 }
 
+/// 处理 get 子命令
 async fn get(client: Client, args: &Get) -> Result<()> {
     let resp = client.get(&args.url).send().await?;
-    println!("{:?}", resp.text().await?);
-    Ok(())
+    Ok(print_resp(resp).await?)
 }
 
+/// 处理 post 子命令
 async fn post(client: Client, args: &Post) -> Result<()> {
     let mut body = HashMap::new();
     for pair in args.body.iter() {
         body.insert(&pair.k, &pair.v);
     }
     let resp = client.post(&args.url).json(&body).send().await?;
-    println!("{:?}", resp.text().await?);
+    Ok(print_resp(resp).await?)
+}
+
+/// 打印服务器版本号+状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+/// 打印服务器返回的 http header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    println!("\n");
+}
+
+/// 打印服务器返回的 http body
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        // 其他 mime 类型
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
     Ok(())
+}
+
+/// 将服务器返回的 content-type 解析成 mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url_works() {
+        assert!(parse_url("abc").is_err());
+        assert!(parse_url("http://abc.xyz").is_ok());
+        assert!(parse_url("https://httpbin.org/post").is_ok());
+    }
+    #[test]
+    fn parse_kv_pair_works() {
+        assert!(parse_kv_pair("a").is_err());
+        assert_eq!(
+            parse_kv_pair("a=1").unwrap(),
+            KvPair {
+                k: "a".into(),
+                v: "1".into()
+            }
+        );
+        assert_eq!(
+            parse_kv_pair("b=").unwrap(),
+            KvPair {
+                k: "b".into(),
+                v: "".into()
+            }
+        );
+    }
 }
