4903d6be4b61cdf37762343a66515fa054004281
lyfQAQ <0219lyf@gmail.com>
2024-04-18 10:08:00 +0800

    add custom parse args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use anyhow::{anyhow, Result};
 use clap::{Args, Parser, Subcommand};
+use reqwest::Url;
+use std::str::FromStr;
 
 /// A naive httpie implementation with Rust
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version = "1.0", about, long_about = None)]
 struct Opts {
     #[command(subcommand)]
     subcmd: SubCommand,
@@ -21,6 +24,7 @@ enum SubCommand {
 #[derive(Args, Debug)]
 struct Get {
     /// http 请求的 url
+    #[arg(value_parser = parse_url)]
     url: String,
 }
 
@@ -31,10 +35,45 @@ struct Get {
 #[derive(Args, Debug)]
 struct Post {
     /// http 请求的 url
+    #[arg(value_parser = parse_url)]
     url: String,
     /// http 请求的 body
-    body: Vec<String>,
+    #[arg(value_parser = parse_kv_pair)]
+    body: Vec<KvPair>,
 }
+
+/// 通过 parse_kv_pair 将key=value 解析成 KvPair 结构
+#[derive(Debug, Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+/// 实现 FromStr trait 的类,可以用 parse 方法解析自己
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 将 Some(T)/None 转换为 Ok(T)/Err(E)
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+// 自定义解析函数
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
 fn main() {
     let opts: Opts = Opts::parse();
     println!("{:?}", opts);
