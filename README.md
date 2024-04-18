255bc5974842834df0948be5d70ca945197fd043
lyfQAQ <0219lyf@gmail.com>
2024-04-18 09:42:15 +0800

    add cmd arg parse

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # httpie
-- 命令行解析
+- 命令行解析: [clap](https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_2/index.html#subcommands)
 - 发送 http 请求
 - 用户友好的方式输出
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use clap::{Args, Parser, Subcommand};
+
+/// A naive httpie implementation with Rust
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    subcmd: SubCommand,
+}
+
+// 子命令对应不同的 http 方法
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+// get 子命令
+
+/// feed get with an url and we will retrieve the response for you
+#[derive(Args, Debug)]
+struct Get {
+    /// http 请求的 url
+    url: String,
+}
+
+// post 子命令需要输入一个 URL，和若干个可选的 key=value，用于提供 json body
+
+/// feed post with an url and optional key=value pairs. We will post the data
+/// as JSON, and retrieve the response for you
+#[derive(Args, Debug)]
+struct Post {
+    /// http 请求的 url
+    url: String,
+    /// http 请求的 body
+    body: Vec<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
 }
